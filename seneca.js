5c3e2d160ac0af5dd5ae255463440d146b480220
RayGervais <raygervais@hotmail.ca>
2017-03-24 17:09:13 -0400

    Added new Unit-Tests

--- seneca.js
+++ seneca.js
@@ -1,10 +1,11 @@
 /**
  * Given a string `email`, return `true` if the string is in the form
  * of a valid Seneca College email address, `false` othewise.
+ * @param {email} email address to be validated
+ * @return {boolean} Indicating successful validation (true), or false.
  */
 exports.isValidEmail = function(email) {
-  var emailDomain = "myseneca.ca";
-  if (/^\w+([\.-]?\w+)*@(myseneca.ca)/.test(email)) {
+  if (/^\w+([\.-]?\w+)*@\w*(seneca)\w*(.ca)/.test(email)) {
       return (true);
   }
   return (false);
@@ -13,8 +14,14 @@ exports.isValidEmail = function(email) {
 /**
  * Given a string `name`, return a formatted Seneca email address for
  * this person. NOTE: the email doesn't need to be real/valid/active.
+ * @param {name} name is the argument / username
+ * @return {String} String containing the formatted email address.
  */
 exports.formatSenecaEmail = function(name) {
-  name.trim();
-  return name.concat('@myseneca.ca');
+  if (name != null) {
+    name.trim();
+    return name.concat('@myseneca.ca');
+  }
+
+  return "Error: Invalid Argument";
 };
--- seneca.test.js
+++ seneca.test.js
@@ -1,17 +1,38 @@
-var seneca = require('./seneca');
+let seneca = require('./seneca');
 
 describe('seneca.isValidNode()', function() {
     test('isValidEmail returns true for simple myseneca address', function() {
-        var simpleEmail = 'someone@myseneca.ca';
-        expect(seneca.isValidEmail(simpleEmail)).toBe(true);
+        expect(seneca.isValidEmail('someone@myseneca.ca')).toBe(true);
     });
 
-    test('Returns falise for a invalid myseneca address', function() {
-        var invalidEmail = 'someone@hotmail.ca';
-        expect(seneca.isValidEmail(invalidEmail)).toBe(false);
+    test('Returns true for a staff email address', function () {
+        expect(seneca.isValidEmail('david.humphrey@senecacollege.ca')).toBe(true);
+    });
+
+    test('Returns false for a invalid myseneca address', function() {
+        expect(seneca.isValidEmail('someone@hotmail.ca')).toBe(false);
+    });
+
+    test('Returns false for a malformed myseneca address', function() {
+        expect(seneca.isValidEmail('  rmgervais@senecacollege.ca')).toBe(false);
+    });
+
+    test('Returns false for invalid argument being passed', function() {
+        expect(seneca.isValidEmail(123)).toBe(false);
+    });
+
+    test('Returns false for an array argument being passed', function() {
+        expect(seneca.isValidEmail([1, 'rmgervais@myseneca.ca'])).toBe(false);
     });
 });
-test('formatSenecaEmail returns the formatted email address', function() {
-    var simpleName = "rmgervais";
-    expect(seneca.formatSenecaEmail(simpleName)).toBe("rmgervais@myseneca.ca");
+
+describe('seneca.formatSenecaEmail()', function() {
+    test('formatSenecaEmail returns the formatted email address', function() {
+        expect(seneca.formatSenecaEmail('rmgervais')).toBe('rmgervais@myseneca.ca');
+    });
+
+    test('Returns a thrown error due to name being null', function() {
+        expect(seneca.formatSenecaEmail()).toBe("Error: Invalid Argument");
+    });
 });
+
